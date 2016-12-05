4c4c859232ca6754ec638e0975efbbce5eaa9876
Alfonzork <alfonzork@hotmail.com>
2016-12-05 19:24:40 -0300

    cliente crear actualizar y borrar

--- DEJ Mobile/src/java/DAO/ClienteDAO.java
+++ DEJ Mobile/src/java/DAO/ClienteDAO.java
@@ -5,10 +5,64 @@
  */
 package DAO;
 
+import DAL.HibernateUtil;
+import org.hibernate.Session;
+
+
 /**
  *
  * @author Alfonzork
  */
 public class ClienteDAO {
     
+    public boolean Add(ENTITY.Cliente c) throws Exception
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            session.save(c);
+            session.getTransaction().commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            session.close();
+            System.err.println(e.getMessage());
+            throw e;
+        }
+    }
+    
+    public boolean Update(ENTITY.Cliente c) throws Exception
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            session.update(c);
+            session.getTransaction().commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            session.close();
+            System.err.println(e.getMessage());
+            throw e;
+        }
+    }
+    
+    public boolean Delete(ENTITY.Cliente c) throws Exception
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            session.delete(c);
+            session.getTransaction().commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            session.close();
+            System.err.println(e.getMessage());
+            throw e;
+        }
+    }
 }
